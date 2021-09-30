b723c4652bbfe6381550fe11f19b1489153d922e
Rohit <rkumar8092378845@gmail.com>
2021-09-30 17:45:00 +0530

    using InitBinder annotation to validate a String if it has whitespaces in it

--- src/main/java/com/rohitthebest/springdemo/mvc/CustomerController.java
+++ src/main/java/com/rohitthebest/springdemo/mvc/CustomerController.java
@@ -2,9 +2,12 @@ package com.rohitthebest.springdemo.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,6 +15,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/customer")
 public class CustomerController {
 
+	
+	// add an initBinder ... to convert trim input string
+	// removing leading and trailing whitespace
+	// resolve issue for our validation
+	
+	/**
+	 * This will pre process every data which is coming to this controller 
+	 * and if it is String and the String has whiteSpace in the starting 
+	 * and ending it will just trim it, also as we
+	 * have passed true into the StringTrimmerEditor as parameter,
+	 * if the String contains only whiteSpaces then it will make it null
+	 * and thus it will be catched by our validator
+	 */
+	
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
+	
+	
 	@RequestMapping("/showForm")
 	public String showForm(Model theModel) {
 
